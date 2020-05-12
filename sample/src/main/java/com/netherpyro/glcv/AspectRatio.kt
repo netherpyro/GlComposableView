19caa5cde4f8b58808be46949035fdf16b16ef09
mmikhailov <netherpyro@gmail.com>
2020-05-12 22:38:04 +0600

    [sample] rearrange aspect ratio items

--- sample/src/main/java/com/netherpyro/glcv/AspectRatio.kt
+++ sample/src/main/java/com/netherpyro/glcv/AspectRatio.kt
@@ -5,13 +5,13 @@ package com.netherpyro.glcv
  */
 enum class AspectRatio(val value: Float, val title: String) {
 
-    RATIO_1_1(1f, "1:1"),
-    RATIO_5_4(5f / 4f, "5:4"),
-    RATIO_3_2(3f / 2f, "3:2"),
-    RATIO_16_9(16f / 9f, "16:9"),
     RATIO_18_9(18f / 9f, "18:9"),
-    RATIO_9_18(9f / 18f, "9:18"),
-    RATIO_9_16(9f / 16f, "9:16"),
+    RATIO_16_9(16f / 9f, "16:9"),
+    RATIO_3_2(3f / 2f, "3:2"),
+    RATIO_5_4(5f / 4f, "5:4"),
+    RATIO_1_1(1f, "1:1"),
+    RATIO_4_5(4f / 5f, "4:5"),
     RATIO_2_3(2f / 3f, "2:3"),
-    RATIO_4_5(4f / 5f, "4:5")
+    RATIO_9_16(9f / 16f, "9:16"),
+    RATIO_9_18(9f / 18f, "9:18")
 }
