27c2b8d1d09f6c775c47d40cb0e99841acf9de76
Alexey Korshun <lehakorshun@gmail.com>
2020-06-17 14:17:31 +0600

    extract throttler to separate class

--- library/src/main/java/com/netherpyro/glcv/util/HapticUtil.kt
+++ library/src/main/java/com/netherpyro/glcv/util/HapticUtil.kt
@@ -1,7 +1,6 @@
 package com.netherpyro.glcv.util
 
 import android.content.Context
-import android.os.Handler
 import com.netherpyro.glcv.extensions.vibrate
 
 /**
@@ -16,27 +15,4 @@ internal class HapticUtil(
     fun vibrate() {
         throttler.publish { context.vibrate() }
     }
-
-    private class Throttler(private val thresholdMs: Long = 300) {
-
-        companion object {
-            private const val WHAT = 7879
-        }
-
-        private val handler: Handler = Handler()
-
-        fun publish(block: () -> Unit) {
-            if (handler.hasMessages(WHAT)) {
-                return
-            }
-
-            block.invoke()
-            handler.sendMessageDelayed(handler.obtainMessage(WHAT), thresholdMs)
-        }
-
-        fun clear() {
-            handler.removeCallbacksAndMessages(null) // null means all
-        }
-    }
-
 }
